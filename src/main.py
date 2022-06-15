9aa88cab43488cfef27f095ae3c8f0614ef7ac34
Jon Whittlestone <dev@howapped.com>
2022-06-15 16:46:50 +0100

    Basic template with StaticFiles

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 from typing import Union
+from starlette.templating import Jinja2Templates
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
+templates = Jinja2Templates("templates")
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/welcome")
+def welcome(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/items/{item_id}")
